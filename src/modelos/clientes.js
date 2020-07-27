ef2de5ccd0e393e84969363676844b48b6d8217b
arisoftapp <repositorios@arisoft.com.mx>
2020-07-27 17:39:47 -0600

    insertar cliente

---
+++ src/modelos/clientes.js
@@ -0,0 +1,18 @@
+var con = require('../conexion_db');
+
+let userModel = {};
+
+userModel.insertCliente = (data, callback) => {
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = "INSERT INTO clientes (nom_cliente, puesto, comentario) VALUES (" + data.nombre + ", " + data.puesto + "," + data.comentario + ")";
+        con.query(sql, function(err, result) {
+            if (err) throw err;
+            callback(null, result.message);
+            console.log("1 record inserted");
+        });
+    });
+
+};
+module.exports = userModel;
---
+++ src/rutas/clientes.js
@@ -0,0 +1,28 @@
+const model = require('../modelos/clientes');
+const jwt = require('jsonwebtoken');
+module.exports = function(app, rutasprotegidas) {
+
+    app.get('/insertarCliente', rutasprotegidas, (req, res) => {
+        var datos = {
+            "nombre": req.body.nombre,
+            "puesto": req.body.puesto,
+            "comentario": req.body.comentario
+        }
+        model.insertCliente(datos, (err, data) => {
+            if (err) {
+                res.status(500).send({
+                    success: false,
+                    mensaje: 'Error al buscar insertar cliente:' + err
+                });
+            } else {
+                res.json({
+                    success: true,
+                    respuesta: data,
+                    mensaje: "consulta con exito"
+
+                })
+
+            }
+        });
+    });
+}
