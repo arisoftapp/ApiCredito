b1c199daced3e9ece6e262771b3d205150f4cc07
arisoftapp <repositorios@arisoft.com.mx>
2020-07-28 09:42:25 -0600

    query

--- src/modelos/clientes.js
+++ src/modelos/clientes.js
@@ -4,22 +4,19 @@ let userModel = {};
 
 userModel.insertCliente = (data, callback) => {
     console.log(data.nombre);
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        var sql = "INSERT INTO clientes (nom_cliente, puesto, comentarios) VALUES ('" + data.nombre + "', '" + data.puesto + "','" + data.comentario + "')";
-        con.query(sql, function(err, result) {
-            if (err) {
-                callback(err, null);
-                //throw err;
-            } else {
+    var sql = "INSERT INTO clientes (nom_cliente, puesto, comentarios) VALUES ('" + data.nombre + "', '" + data.puesto + "','" + data.comentario + "')";
+    con.query(sql, function(err, result) {
+        if (err) {
+            callback(err, null);
+            //throw err;
+        } else {
 
-                console.log("1 record inserted " + result);
-                callback(null, result.message);
-            }
+            console.log("1 record inserted " + result);
+            callback(null, result.message);
+        }
 
-        });
     });
+
 
 };
 module.exports = userModel;
