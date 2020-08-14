258f6bd3b7a19e79a919ea4860cb7ccd2a9b87e8
arisoftapp <repositorios@arisoft.com.mx>
2020-08-14 14:10:04 -0600

    cliente

--- src/rutas/clientes.js
+++ src/rutas/clientes.js
@@ -42,19 +42,29 @@ module.exports = function(app, rutasprotegidas) {
     });
     app.get('/cliente/:codigoMacro', rutasprotegidas, (req, res) => {
 
-        model.getCliente(req.params.idcliente, (err, data) => {
+        model.getCliente(req.params.codigoMacro, (err, data) => {
             if (err) {
                 res.status(500).send({
                     success: false,
                     mensaje: 'Error al buscar clientes:' + err
                 });
             } else {
-                res.json({
-                    success: true,
-                    respuesta: data,
-                    mensaje: "consulta con exito"
+                if (data.length < 1) {
+                    res.json({
+                        success: true,
+                        respuesta: data,
+                        mensaje: "No encontro cliente"
 
-                })
+                    })
+                } else {
+                    res.json({
+                        success: true,
+                        respuesta: data,
+                        mensaje: "consulta con exito"
+
+                    })
+                }
+
 
             }
         });
